264c96ee6a714865b49bd55aad8ed33295ee19e3
sub33r0 <z3rowords@gmail.com>
2024-04-10 00:19:50 +0300

    Add search

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
+import { SearchComponent } from './components/partials/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    StarRatingComponent,
+    SearchComponent,
+    
+
   ],
   imports: [
     BrowserModule,
--- client/src/app/components/pages/home/home.component.ts
+++ client/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PCService } from 'src/app/services/pc.service';
 import { PC } from 'src/app/shared/models/PC';
 
@@ -11,8 +12,14 @@ export class HomeComponent implements OnInit{
 
   parts:PC[] = [];
 
-  constructor(private pcService: PCService) { 
-    this.parts = this.pcService.getAll();
+  constructor(private pcService: PCService, activatedRoute: ActivatedRoute) { 
+    activatedRoute.params.subscribe((params) => {
+      if(params.searchTerm){
+        this.parts = this.pcService.getAllPcPartsBySearchTerm(params.searchTerm);
+      } else {
+        this.parts = this.pcService.getAll();
+      }
+    });
   }
 
   ngOnInit(): void {
